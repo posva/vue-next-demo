64812ba9967f6c6a81c70cb74fcddd8d8eb99943
Eduardo San Martin Morote <posva13@gmail.com>
2020-05-04 10:21:32 +0200

    use only vuex

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div class="home">Message: {{ message }}</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { defineComponent, computed } from 'vue'
 
-export default {
+export default defineComponent({
   name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  props: {
+    message: {
+      required: true,
+      type: String,
+    },
+  },
+
+  setup(props) {
+    const name = computed(() => props.message)
+
+    return { name }
+  },
+})
 </script>
